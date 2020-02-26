4960e3f56f03fef4b144fad6c555ddce0a7fc384
Emieldv <emielvlees@hotmail.com>
2020-02-26 18:28:11 +0100

    rectangle

---
+++ rect_animated.py
@@ -0,0 +1,71 @@
+from sense_hat import SenseHat
+from time import sleep
+import sys
+
+r = (255, 0, 0)
+e = (0, 0, 0)
+
+image = [
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,r,r,e,e,e,
+e,e,e,r,r,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+]
+image2 = [
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+e,e,r,r,r,r,e,e,
+e,e,r,e,e,r,e,e,
+e,e,r,e,e,r,e,e,
+e,e,r,r,r,r,e,e,
+e,e,e,e,e,e,e,e,
+e,e,e,e,e,e,e,e,
+]
+image3 = [
+e,e,e,e,e,e,e,e,
+e,r,r,r,r,r,r,e,
+e,r,e,e,e,e,r,e,
+e,r,e,e,e,e,r,e,
+e,r,e,e,e,e,r,e,
+e,r,e,e,e,e,r,e,
+e,r,r,r,r,r,r,e,
+e,e,e,e,e,e,e,e,
+]
+image4 = [
+r,r,r,r,r,r,r,r,
+r,e,e,e,e,e,e,r,
+r,e,e,e,e,e,e,r,
+r,e,e,e,e,e,e,r,
+r,e,e,e,e,e,e,r,
+r,e,e,e,e,e,e,r,
+r,e,e,e,e,e,e,r,
+r,r,r,r,r,r,r,r,
+]
+
+def main():
+    while True:
+        sense.set_pixels(image)
+        sleep,(0.5)
+        sense.set_pixels(image2)
+        sleep(0.5)
+        sense.set_pixels(image3)
+        sleep(0.5)
+        sense.set_pixels(image4)
+        sleep(0.5)
+        sense.set_pixels(image3)
+        sleep(0.5)
+        sense.set_pixels(image2)
+        sleep(0.5)
+
+try:
+    main()
+except (KeyboardInterrupt, SystemExit):
+    print("programma sluiten")
+finally:
+    print("opkuisen van de matrix")
+    sense.clear()
+    sys.exit(0)
