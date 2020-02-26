0787d6382196a63be38d60e8af9e02333112ed3e
Emieldv <51248317+Emieldv@users.noreply.github.com>
2020-02-26 11:03:49 +0100

    oefening 1 klaar!

---
+++ nmd.py
@@ -0,0 +1,29 @@
+import time
+import random
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.set_rotation(90)
+
+def main():
+    while True:
+        sense.show_letter("N", [random.randint(1,256),random.randint(1,256),random.randint(1,256)])
+        time.sleep(1)
+        
+        sense.show_letter("M", [random.randint(1,256),random.randint(1,256),random.randint(1,256)])
+        time.sleep(1)
+        
+        sense.show_letter("D", [random.randint(1,256),random.randint(1,256),random.randint(1,256)])
+        time.sleep(1)
+        
+        sense.clear()
+        time.sleep(3)
+    
+try:
+    main()
+except (KeyboardInterrupt, SystemExit):
+    print("programma sluiten")
+finally:
+    print("opkuisen van de matrix")
+    sense.clear()
+    sys.exit(0)
