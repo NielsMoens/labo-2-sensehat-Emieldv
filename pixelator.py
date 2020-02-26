b8a56b3fccf92ddecb4215743ce863c41d50d98e
Emieldv <emielvlees@hotmail.com>
2020-02-26 13:54:33 +0100

    test

---
+++ pixelator.py
@@ -0,0 +1,18 @@
+from sense_hat import SenseHat
+import sys
+import time
+import random
+
+matrix2 = 0
+
+def main():
+    while matrix2 < 7:
+        
+try:
+    main()
+except (KeyboardInterrupt, SystemExit):
+    print("programma sluiten")
+finally:
+    print("opkuisen van de matrix")
+    sense.clear()
+    sys.exit(0)
