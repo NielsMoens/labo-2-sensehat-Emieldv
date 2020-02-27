7c688efa2ce907576ce48c249c8127ced6cad529
emieldv <emielvlees@hotmail.com>
2020-02-27 16:38:48 +0100

    test

--- avatar_animated.py
+++ avatar_animated.py
@@ -6,15 +6,15 @@ import sys
 sense = SenseHat()
 sense.set_rotation(90)
 
-image1 = Image.open("img/avatar1.jpg")
-image2 = Image.open("img/avatar2.jpg")
+# image1 = Image.open("img/avatar1.jpg")
+# image2 = Image.open("img/avatar2.jpg")
 
 sense.set_pixels("img/avatar6.jpg")
 
-def main():
-    while True:
-        sense.set_pixels(image2)
-        sleep(2)
+# def main():
+#     while True:
+#         sense.set_pixels(image2)
+#         sleep(2)
 
 try:
     main()
